3123807c20ed843676a2115b5b79ef6db089e44c
Kirill <apatski94@gmail.com>
2023-03-22 00:22:55 +0300

    Added component Input

---
+++ src/common/Input/Input.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent} from 'react'
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+
+type InputTextPropsType = DefaultInputPropsType & {
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  error?: string
+}
+
+export const Input: React.FC<InputTextPropsType> = (
+  {
+    type,
+    onChange, onChangeText,
+    onKeyPress, onEnter,
+    className,
+    ...restProps
+  }
+) => {
+
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e)
+    onChangeText && onChangeText(e.currentTarget.value)
+  }
+  const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+    onKeyPress && onKeyPress(e);
+
+    onEnter
+    && e.key === 'Enter'
+    && onEnter()
+  }
+
+  return (
+    <>
+      <input
+        type={'text'}
+        onChange={onChangeCallback}
+        onKeyPress={onKeyPressCallback}
+        {...restProps}
+      />
+    </>
+  )
+}
+
+export default Input
