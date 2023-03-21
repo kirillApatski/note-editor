ebe1a777cb2e408e921197f087b8ef1e8f3c91f9
Kirill <apatski94@gmail.com>
2023-03-22 00:21:51 +0300

    Added logic CRUD

--- src/Component/Main/Main.tsx
+++ src/Component/Main/Main.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import s from './Main.module.scss'
 import Note from "./Note/Note";
+import {addNoteAC, deleteNoteAC, noteReducer, updateNoteTextAC} from "../../store/reducers/noteReduser";
+import {store} from "../../store/store";
+import Button from "../../common/Button/Button";
+import Input from "../../common/Input/Input";
 
 
 const Main = () => {
+  const [state, dispatch] = useReducer(noteReducer, store)
+  const [valueInputText, setValueInputText] = useState('')
+
+  const onChangeHandle = (text: string) => {
+    setValueInputText(text)
+  }
+  const addNewNote = () => {
+    valueInputText && dispatch(addNoteAC(valueInputText))
+    setValueInputText('')
+  }
+  const onKeyPressHandler = () => {
+    addNewNote()
+  }
+  const deleteNote = (id: string) => {
+    dispatch(deleteNoteAC(id))
+  }
+
+  const updateNoteText = (newText: string, id: string) => {
+    dispatch(updateNoteTextAC(newText, id))
+  }
 
   return (
     <main className={s.main}>
-      <div className={s.addNote}>
+      <form className={s.addNote}>
         <label htmlFor="addNote">Added Note</label>
-        <input form="addNote" type="text"/>
-        <button>Add</button>
-      </div>
+        <Input
+          form="addNote"
+          type="text"
+          onChangeText={onChangeHandle}
+          onEnter={onKeyPressHandler}
+          value={valueInputText}
+        />
+        <Button onClick={addNewNote}>Add</Button>
+      </form>
       <div className={s.mainContent}>
-          <Note/>
+        {
+          !state.length
+            ?
+            <h2 className={s.contentTitle}>Create a note</h2>
+            : state.map(note => {
+              return (
+                <Note
+                  key={note.id}
+                  id={note.id}
+                  deleteNote={deleteNote}
+                  updateNoteText={updateNoteText}
+                  text={note.text}
+                />
+              )
+            })
+        }
       </div>
     </main>
   );
--- src/Component/Main/Note/Note.tsx
+++ src/Component/Main/Note/Note.tsx
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import s from './Note.module.scss'
-import DeleteIcon from "../../../assets/icons/deleteIcon";
-import EditIcon from "../../../assets/icons/editIcon";
+import DeleteIcon from "../../../assets/icons/DeleteIcon";
+import EditIcon from "../../../assets/icons/EditIcon";
+import Button from "../../../common/Button/Button";
+import ConfirmIcon from "../../../assets/icons/ConfirmIcon";
 
-const Note = () => {
+type NotePropsType = {
+  text: string
+  id: string
+  deleteNote: (id: string) => void
+  updateNoteText: (text: string, id: string) => void
+}
+
+const Note: FC<NotePropsType> = ({text, id, deleteNote, updateNoteText}) => {
+  const [editMode, setEditMode] = useState(false)
+  const [valueTextarea, setValueTextarea] = useState(text)
+
+  const onChangeHandlerTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const textareaValue = e.currentTarget.value
+    setValueTextarea(textareaValue)
+    updateNoteText(textareaValue, id)
+  }
+  const onClickHandlerDelete = () => {
+    deleteNote(id)
+  }
+  const onClickHandlerEditMode = (mode: boolean) => {
+    setEditMode(mode)
+  }
   return (
     <div className={s.noteWrapper}>
       <div className={s.notePanel}>
-        <DeleteIcon/>
-        <EditIcon/>
+        <Button
+          onClick={onClickHandlerDelete}
+          className={s.button}
+        >
+          <DeleteIcon/>
+        </Button>
+        <Button
+          onClick={() => onClickHandlerEditMode(true)}
+          className={s.button}
+        >
+          <EditIcon/>
+        </Button>
       </div>
+      {
+        !editMode
+          ? <p className={s.noteText}>{text}</p>
+          : <div className={s.updateBox}>
+            <textarea
+              autoFocus={true}
+              className={s.textarea}
+              value={valueTextarea}
+              onChange={onChangeHandlerTextarea}
+            />
+            <Button
+              className={s.button}
+              onClick={() => onClickHandlerEditMode(false)}
+            >
+              <ConfirmIcon/>
+            </Button>
+          </div>
+      }
+
     </div>
   );
 };
