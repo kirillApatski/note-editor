87b3ae94069896e774785024314e9114de4f979c
Kirill <apatski94@gmail.com>
2023-03-22 00:18:01 +0300

    Added Button Component and Button styles

---
+++ src/common/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from './Button.module.scss'
+
+
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+type ButtonPropsType = DefaultButtonPropsType & {
+  red?: boolean
+}
+
+const Button: React.FC<ButtonPropsType> = (
+  {
+    className,
+    ...restProps
+  }
+) => {
+  const finalClassName = className ? className : s.button
+
+  return (
+    <button
+      type='button'
+      className={finalClassName}
+      {...restProps}
+    />
+  )
+}
+
+export default Button
