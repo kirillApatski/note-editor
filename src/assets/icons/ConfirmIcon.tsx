b7b46a98b304552ef1610fd6d7c26a1993aac2cc
Kirill <apatski94@gmail.com>
2023-03-22 00:19:31 +0300

    Added component ConfirmIcon

---
+++ src/assets/icons/ConfirmIcon.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const ConfirmIcon = () => {
+  return (
+    <svg fill="#000000" height="30px" width="30px" version="1.1" id="Capa 1" xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 128.411 128.411" >
+<g>
+	<g>
+		<polygon points="127.526,15.294 45.665,78.216 0.863,42.861 0,59.255 44.479,113.117 128.411,31.666 		"/>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+  <g>
+	</g>
+</g>
+</svg>
+  );
+};
+
+export default ConfirmIcon;
