e65600419cb9e190ef67761a156a08e6e8873564
Kirill <apatski94@gmail.com>
2023-03-24 18:55:12 +0300

    added component DeleteIconMin

---
+++ src/assets/icons/DeleteIconMin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeleteIconMin = () => {
+  return (
+    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16 9L10 15M10.0002 9L16.0002 15M8 18L2 12L8 6C8 6 10 5.5 13.5 5.5C19.1685 5.5 20.5 5.5 20.5 12C20.5 18.5 19.2925 18.5 13.5 18.5C10 18.5 8 18 8 18Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default DeleteIconMin;
