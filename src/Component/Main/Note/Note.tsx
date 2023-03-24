7461beb7379a99a982fd057e5156acd4dc0d94fc
Kirill <apatski94@gmail.com>
2023-03-24 18:53:16 +0300

    added logic addTag  and deleteTag

--- src/Component/Main/Note/Note.tsx
+++ src/Component/Main/Note/Note.tsx
@@ -1,37 +1,59 @@
-import React, {ChangeEvent, FC, useState} from 'react';
+import React, {ChangeEvent, FC, useReducer, useState} from 'react';
 import s from './Note.module.scss'
 import DeleteIcon from "../../../assets/icons/DeleteIcon";
 import EditIcon from "../../../assets/icons/EditIcon";
 import Button from "../../../common/Button/Button";
 import ConfirmIcon from "../../../assets/icons/ConfirmIcon";
+import {addTagsAC, deleteTagAC, tagsReducer, TagType} from "../../../reducers/tagsReducer";
+import {v1} from "uuid";
+import DeleteIconMin from "../../../assets/icons/DeleteIconMin";
 
 type NotePropsType = {
   text: string
-  id: string
+  idNote: string
   deleteNote: (id: string) => void
   updateNoteText: (text: string, id: string) => void
 }
 
-const Note: FC<NotePropsType> = ({text, id, deleteNote, updateNoteText}) => {
+const Note: FC<NotePropsType> = ({text, idNote, deleteNote, updateNoteText}) => {
   const [editMode, setEditMode] = useState(false)
   const [valueTextarea, setValueTextarea] = useState(text)
+  const [tags, dispatch] = useReducer(tagsReducer, {})
 
   const onChangeHandlerTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const textareaValue = e.currentTarget.value
     setValueTextarea(textareaValue)
-    updateNoteText(textareaValue, id)
+    updateNoteText(textareaValue, idNote)
   }
-  const onClickHandlerDelete = () => {
-    deleteNote(id)
+  const onClickHandlerDeleteNote = () => {
+    deleteNote(idNote)
+  }
+  const onClickHandlerDeleteTag = (idTag: string) => {
+    dispatch(deleteTagAC(idNote, idTag))
   }
   const onClickHandlerEditMode = (mode: boolean) => {
     setEditMode(mode)
+    addTeg(valueTextarea)
+  }
+  const addTeg = (value: string) => {
+    let val = value.split(/(#[a-z\d-]+)/ig);
+    const arr = []
+    for (let i = 0; i < val.length; i++) {
+      if (val[i].charAt(0) === "#") {
+        const obg = {
+          tag: val[i],
+          id: v1()
+        } as TagType
+        arr.push(obg)
+        dispatch(addTagsAC(arr, idNote))
+      }
+    }
   }
   return (
     <div className={s.noteWrapper}>
       <div className={s.notePanel}>
         <Button
-          onClick={onClickHandlerDelete}
+          onClick={onClickHandlerDeleteNote}
           className={s.button}
         >
           <DeleteIcon/>
@@ -45,7 +67,23 @@ const Note: FC<NotePropsType> = ({text, id, deleteNote, updateNoteText}) => {
       </div>
       {
         !editMode
-          ? <p className={s.noteText}>{text}</p>
+          ? <>
+            <p className={s.noteText}>{text}</p>
+            {
+              tags[idNote]?.map(tag => {
+                return (
+                  <ul key={tag.id} className={s.tagsBox}>
+                    <li>
+                      {tag.tag}
+                    </li>
+                    <Button className={s.button} onClick={() =>onClickHandlerDeleteTag(tag.id)}>
+                      <DeleteIconMin/>
+                    </Button>
+                  </ul>
+                )
+              })
+            }
+          </>
           : <div className={s.updateBox}>
             <textarea
               autoFocus={true}
@@ -61,7 +99,6 @@ const Note: FC<NotePropsType> = ({text, id, deleteNote, updateNoteText}) => {
             </Button>
           </div>
       }
-
     </div>
   );
 };
