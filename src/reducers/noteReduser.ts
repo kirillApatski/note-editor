6fea59a9ce02112b1f6916d15c4c5d90734aa4d7
Kirill <apatski94@gmail.com>
2023-03-24 18:52:20 +0300

    refactoring

--- src/reducers/noteReduser.ts
+++ src/reducers/noteReduser.ts
@@ -1,36 +1,36 @@
-import {StateType} from "../store";
 import {v1} from "uuid";
+import {StateType} from "../Component/Main/Main";
 
 
 export const noteReducer = (state: StateType[], action: ActionType): StateType[] => {
   switch (action.type) {
     case 'ADD-NOTE':
       const newNote = {
-        id: v1(),
+        id: action.id,
         text: action.text,
-        // teg: ''
+        tags: {}
       }
     return [
         ...state,
         newNote
     ]
-    case "DELETE-NOTE":
-      return state.filter(note => note.id !== action.id)
-    case "UPDATE-NOTE-TEXT":
-      return state.map(note => note.id === action.id ? {...note, text: action.text} : note)
+    case 'DELETE-NOTE':
+      return state.filter(note => note.id !== action.idNote)
+    case 'UPDATE-NOTE-TEXT':
+      return state.map(note => note.id === action.idNote ? {...note, text: action.text} : note)
     default:
       return state
   }
 }
 
 export const addNoteAC = (text: string) => {
-  return {type: 'ADD-NOTE', text} as const
+  return {type: 'ADD-NOTE', text, id: v1()} as const
 }
-export const deleteNoteAC = (id: string) => {
-  return {type: 'DELETE-NOTE', id} as const
+export const deleteNoteAC = (idNote: string) => {
+  return {type: 'DELETE-NOTE', idNote} as const
 }
-export const updateNoteTextAC = (text: string, id: string) => {
-  return {type: 'UPDATE-NOTE-TEXT', text, id} as const
+export const updateNoteTextAC = (text: string, idNote: string) => {
+  return {type: 'UPDATE-NOTE-TEXT', text, idNote} as const
 }
 
 
